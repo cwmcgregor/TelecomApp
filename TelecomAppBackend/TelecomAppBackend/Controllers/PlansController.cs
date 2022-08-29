041a1dab7dc43c4debc24e200c99b5f42fe2b80d
SyedAli-07 <110481401+SyedAli-07@users.noreply.github.com>
2022-08-29 00:16:57 -0500

    Update:Auth Controller & Added Authentication Middleware

--- TelecomAppBackend/TelecomAppBackend/Controllers/PlansController.cs
+++ TelecomAppBackend/TelecomAppBackend/Controllers/PlansController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,8 @@ namespace TelecomAppBackend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    //[Authorize]
+    //Use [AllowAnonymous] to remove authorization on http request.
     public class PlansController : ControllerBase
     {
         private readonly TelecomDbContext _context;
